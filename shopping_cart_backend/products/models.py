8755309ac9b7c2d5c83db272aa2c03a4d4e21720
corina <pixmin@outlook.com>
2023-02-12 00:56:38 +0800

    Create new product model and migrate table

--- shopping_cart_backend/products/models.py
+++ shopping_cart_backend/products/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+  productname = models.CharField(max_length=255)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
